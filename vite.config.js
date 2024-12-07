994686935083dcf769e76538ad8c0f6cd907eaeb
cejimenez0123 <christianjimenez0123@gmail.com>
2024-12-07 12:02:37 -0500

    npm install gh-pages --save-dev

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],assetsInclude:["**/privacy.html"]
+  plugins: [react()],assetsInclude:["**/privacy.html"],
+  base: '/pb-web' 
   // define: {
   //   'process.env': {}
   // }
