c778bbf3ec59c07bf2d709f9f2836efcf1434fc6
cejimenez0123 <christianjimenez0123@gmail.com>
2024-12-17 10:47:58 -0500

    last

--- src/container/auth/SignUpContainer.jsx
+++ src/container/auth/SignUpContainer.jsx
@@ -7,9 +7,26 @@ export default function SignUpContainer(props){
     const location = useLocation();
     const [token, setToken] = useState('');
     const [password,setPassword]=useState("")
+    const [username,setUsername]=useState("")
     const [confirmPassword,setConfirmPassword]=useState("")
-    const [form, setForm] = useState({ username: '', password: '' });
-    const dispatch = useDispatch()
+    const [selectedImage, setSelectedImage] = useState(null);
+  const [errorMessage, setErrorMessage] = useState('');
+  const handleFileChange = (e) => {
+    const file = e.target.files[0];
+
+    if (file) {
+      // Check file type
+      if (!file.type.startsWith('image/')) {
+        setErrorMessage('Please upload a valid image file.');
+        setSelectedImage(null);
+        return;
+      }
+
+      setErrorMessage('');
+      setSelectedImage(URL.createObjectURL(file));
+    }
+  };
+     const dispatch = useDispatch()
     useEffect(() => {
         const params = new URLSearchParams(location.search);
         const tokenParam = params.get('token');
@@ -19,12 +36,30 @@ export default function SignUpContainer(props){
       }, [location.search]);
     const completeSignUp=()=>{
         {id,token,password,username,profilePicture,selfStatement,privacy
+       
+    if (file) {
+      // Check file type
+      if (!file.type.startsWith('image/')) {
+            const params = { file: file
+            }
+        }
+    
     }}
+        URL.createObjectURL(file)
+    }
     return(
                 <div  id="log-in">
         <h1 className='text-green-100 poppins pb-4'>Complete Sign Up</h1>
         <div className="max-w-96 mx-auto">
-            
+        <div className='pb-4'>
+            <label className="input poppins text-white border bg-transparent border-white  mt-4 flex items-center gap-2">
+  Username
+  <input type="password" className="grow text-white " 
+         value={username}
+         
+         onChange={(e) => setUsername(e.target.value.trim())}
+        placeholder='*****' />
+</label>
 <div className='pb-4'>
             <label className="input poppins text-white border bg-transparent border-white  mt-4 flex items-center gap-2">
   Password
@@ -43,11 +78,34 @@ export default function SignUpContainer(props){
         placeholder='*****' />
 </label>      
     </div>
+    <input
+    className="file-input"
+        type="file"
+        accept="image/*"
+        onChange={handleFileChange}
+        style={{ marginBottom: '20px' }}
+      />
+
+    {errorMessage && <p style={{ color: 'red' }}>{errorMessage}</p>}
+      {selectedImage && (
+        <div style={{ marginTop: '20px' }}>
+          
+          <img
+          className="mx-auto"
+            src={selectedImage}
+            alt="Selected"
+            style={{ maxWidth: '100%', maxHeight: '300px', borderRadius: '10px' }}
+          />
+        </div>
+      )}
+      
             <button
+            disabled={confirmPassword!==password}
             className='bg-green-600 poppins border-none hover:bg-green-400 text-white font-bold py-2 px-4 mt-4 btn-lg rounded '
                onClick={completeSignUp}
                 
                 variant="contained" >Submit</button>
+            </div>
             </div>     
         </div>)
 }
