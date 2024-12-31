e390a70e27d799443af155d5a6639eecc6e4aa42
cejimenez0123 <christianjimenez0123@gmail.com>
2024-12-31 10:07:31 -0500

    f

--- src/components/page/DashboardItem.jsx
+++ src/components/page/DashboardItem.jsx
@@ -202,7 +202,7 @@ return <Button onClick={()=>{
          onClick={handleApprovalClick}
             
           className={`rounded-none
-          text-xl  text-white mr-4 bg-transparent   `}
+          text-xl  text-white pr-8 bg-transparent   `}
         
          >
              Yea
@@ -221,7 +221,7 @@ return <Button onClick={()=>{
 className="             
       text-white
          rounded-none
-         ml-2
+         pl-8
          text-xl
 
          bg-transparent 
