7a252c535101f5f5aa2e790de9abdf54ada8dd49
cejimenez0123 <christianjimenez0123@gmail.com>
2025-01-22 11:41:36 -0500

    error

--- src/components/Alert.jsx
+++ src/components/Alert.jsx
@@ -25,6 +25,6 @@ strokeLinejoin="round"
 strokeWidth="2"
 d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
 </svg>:null}
-<span className="my-auto  mx-4 whitespace-nowrap ">{error?err:success}</span></div>
+<span className="my-auto  mx-4 whitespace-nowrap ">{error?error:success}</span></div>
 <span/></div></div> :null)
 }
