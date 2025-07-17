20f032a56afbde320d457578c47db8bf95ff6fc8
cejimenez0123 <christianjimenez0123@gmail.com>
2025-07-17 10:04:24 -0400

    update

--- src/core/di.js
+++ src/core/di.js
@@ -18,7 +18,7 @@ import algoliasearch from "algoliasearch";
 // }
 
 const client = algoliasearch(import.meta.env.VITE_ALGOLIA_APP_ID,
-  import.meta.env.VITE_ALGOLIA_API)
+  process.env.ALGOLIA_API)
   
 const config = { apiKey:process.env.FIREBASE_API_KEY,
   authDomain: import.meta.env.VITE_AUTH_DOMAIN,
