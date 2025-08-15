958eabcfd5e8f44b4bbf22d89a1381b886e9dd73
cejimenez0123 <christianjimenez0123@gmail.com>
2025-08-15 13:08:18 -0400

    onboard

--- src/container/auth/SignUpContainer.jsx
+++ src/container/auth/SignUpContainer.jsx
@@ -35,6 +35,7 @@ import setLocalStore from '../../core/setLocalStore';
 import getLocalStore from '../../core/getLocalStore';
 import DeviceCheck from '../../components/DeviceCheck';
 import AppleSignInButton from '../../components/auth/AppleSignInButton';
+import { debounce } from 'lodash';
 
 export default function SignUpContainer(props) {
   const isNative = DeviceCheck()
@@ -126,7 +127,10 @@ export default function SignUpContainer(props) {
       setError("Password and Username can't be empty");
     }
   };
+  const handlePrivate=debounce(()=>{
 
+  setIsPrivate(!isPrivate)
+  },10)
   return (
     
       
@@ -218,24 +222,28 @@ setEmail(email)
     
 
   
-    <div className='flex flex-row'>
+    <div className='flex flex-row'  >
     <IonText className="my-auto">Is Private?</IonText>
     
     {/* Yes/No status */}
-    <IonText className="my-auto min-w-10 ml-4">
+    <div >
+    <IonText 
+    onClick={handlePrivate}
+    className="my-auto min-w-10 ml-4">
       {isPrivate ? "Yes" : "No"}
     </IonText>
-    <input
+    </div>
+     <input
 
     id="ion-cb-1"
       // slot="start"
 type='checkbox'
      
-      className='my-auto rounded-full bg-white border-emerald-500  mx-6'
+      className='my-auto   mx-6'
       checked={isPrivate}
-    onClick={() => setIsPrivate(!isPrivate)}
-      color="success"
-    />
+      onClick={handlePrivate}
+   
+    /> 
    
     </div>
   </div>
@@ -302,190 +310,6 @@ type='checkbox'
   );
 }
 
-// import { useLocation, useNavigate, useParams, useSearchParams } from "react-router-dom";
-// import { useState,useEffect,useRef, useContext, useLayoutEffect } from "react";
-// import { useDispatch } from "react-redux";
-// import { uploadProfilePicture} from "../../actions/ProfileActions";
-// import checkResult from "../../core/checkResult";
-// import Paths from "../../core/paths";
-// import info from "../../images/icons/info.svg"
-// import "../../App.css"
-// import { signUp } from "../../actions/UserActions";
-// import Context from "../../context";
-// import GoogleLogin from "../../components/GoogleLogin";
-// export default function SignUpContainer(props){
-   
-//     const [token, setToken] = useState('');
-//     const [password,setPassword]=useState("")
-//     const navigate = useNavigate()
-//     const [username,setUsername]=useState("")
-//     const searchParams = useSearchParams()
-//     const selectRef = useRef()
-//     const [confirmPassword,setConfirmPassword]=useState("")
-//     const [selectedImage, setSelectedImage] = useState("https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_640.png");
-//     const [selfStatement,setSelfStatement]=useState("")
-//     const [file,setFile]=useState(null)
-//     const {setError,setSuccess,success,seo,setSeo}=useContext(Context)
-//     const [frequency,setFrequency]=useState(1)
-//     const [isPrivate,setIsPrivate]=useState(false)
-//     const [googleID,setGoogleID]=useState(null)
-//     const [email,setEmail]=useState("")
-//     useLayoutEffect(()=>{
-//       let soo = seo
-//       soo.title= "Plumbum (Sign Up) - Your Writing, Your Community"
-//       setSeo(soo)
-    
-//     },[])
-//     const handleFileInput = (e) => {
-//     const img = e.target.files[0];
-
-//     if (img) {
-   
-//       if (!img.type.startsWith('image/')) {
-//         setError('Please upload a valid image file.');
-//         setSuccess(null)
-//         setSelectedImage(null);
-      
-//         return;
-//       }
-//       setFile(img)
-//       setError(null);
-    
-//       setSelectedImage(URL.createObjectURL(img));
-//     }
-//   };
-//   useEffect(()=>{
-
-
-// setToken(token)
-
-
-//     return
-
-//   },[searchParams])
-//      const dispatch = useDispatch()
-
-//     const completeSignUp=()=>{
-//      let toke = searchParams[0].get("token")
-//      if(!toke){
-//       toke = token
-//      }
-//     if( localStorage.getItem("googledrivetoken")||password.length>6&&username.length>3){
-//         if(file){
-//         dispatch(uploadProfilePicture({file:file})).then(res=>checkResult(res,payload=>{
-//                 const{fileName}=payload
-//                 const params = {email,googleId:googleID, token:toke,password,username,frequency,profilePicture:fileName,selfStatement,privacy:isPrivate}
-//                 dispatch(signUp(params))
-
-//                 .then(res=>checkResult(res,payload=>{
-                  
-//                    if(payload.profile){navigate(Paths.myProfile())}else{
-//                    localStoage.setItem("firstTime",payload.firstTime)
-//                     setSuccess(null)
-//                    }
-//                    if(payload.error){
-//                     setError(payload.error)
-//                    }
-//                 },err=>{
-//                     setSuccess(null)
-//                     if(err.message){
-//                       setError(err.mesage)
-//                     }else{
-//                       setError(err)
-//                     }
-//                 }))
-     
-       
-//         })
-//       )
-//       }else{
-//         const params = {email,googleId:googleID,token:toke,password,username,profilePicture:selectedImage,selfStatement,privacy:isPrivate}
-//       dispatch(signUp(params))
-//         .then(res=>checkResult(res,payload=>{
-//             const {profile}=payload
-//             localStorage.setItem("firstTime",payload.firstTime)
-//            if(profile){
-            
-//             navigate(Paths.myProfile())}
-//            else{
-//             setSuccess(null)
-//             setError("Try reusing the link")
-//            }
-          
-//         },err=>{
-
-//               setError(err.mesage)
-          
-    
-//         }))}}else{
-//           setError("Password and Username can't be empty")
-//         }
-      
-//     }
-//     return(
-//                 <div  className=" ">
-
-
-//         <div className=" px-4 my-2  bg-emerald-700 bg-opacity-80 rounded-lg max-w-[96%] md:max-w-[42em] md:px-12 mx-auto">
-//           <div className="flex">
-//         <h2 className='text-green-100 lora-medium text-4xl text-center mx-auto pt-8  px-4 md:pt-24 md:pb-8'>Complete Sign Up</h2>
-//         </div>
-    
-//         <div className='pb-4 mx-auto'>
-   
-//             <label className="input lora-medium text-white border bg-transparent rounded-full h-[4em]  border-white  mt-4 flex items-center ">
-//   Username
-//   <input className="grow text-white mx-2 " 
-//          value={username}
-//          placeholder="username"
-//          onChange={(e) => setUsername(e.target.value.trim())}
-//          />
-// </label>
-// {/* <GoogleLogin signIn={true} onUserSignIn={({       email,
-//                                 name,
-//                                 googleId,
-//                                 driveAccessToken})=>{
-//                                   setEmail(email)
-//                                   setGoogleID(googleId)
-                                  
-//                                 }}/> */}
-// {username.length!=0 && username.length<4?<h6>Minimum username length is 4 characters</h6>:null}    
-// {localStorage.getItem("googledrivetoken")?null:<span>
-
-//             <label className="input lora-medium text-white border bg-transparent  rounded-full h-[4em] border-white  mt-4 flex items-center">
-//   Password
-//   <input type="password" className="grow  mx-2  my-2 text-white " 
-//          value={password}
-         
-//          onChange={(e) => setPassword(e.target.value.trim())}
-//         placeholder='*****' />
-// </label>
-// {localStorage.getItem("googledrivetoken")||password.length==0 || password.length>6?null:<h6>Minimum Password Length is 6 characters</h6>}  
-
-// <label className="input lora-medium text-white border bg-transparent border-white  rounded-full h-[4em] mt-4 flex items-center ">
-//   Confirm Password
-//   <input type="password" className="grow  p-2 mx-2 text-white " 
-//          value={confirmPassword}
-         
-//          onChange={(e) => setConfirmPassword(e.target.value.trim())}
-//         placeholder='*****' />
-// </label>  
-// {password==confirmPassword?null:<h6>Passwords need to match</h6>}
-// </span>  } 
-// <label className="w-full mt-8 flex flex-row justify-between  text-left">
-// <div className="flex  flex-row">
-//   <div className='has-tooltip mx-2'>
-//  <img src={info}/> <span className=' bg-slate-50 text-emerald-800 rounded-lg p-2 is-tooltip'>Would you like to be hidden from search?</span>
-// </div>
-//     <span className="text-white text-l open-sans-medium">Will your account be private?</span>    </div>
-//     <div className="flex flex-row">
-// <h6 className="mx-2  my-auto text-white ">{isPrivate?"Yes":"No"}</h6>
-// <input value={isPrivate} onChange={(e)=>setIsPrivate(e.target.checked)}
-//     type="checkbox" className="toggle my-auto toggle-success"  />
-
-// </div>
-
-// </label>
 
 
 //     </div>
