0acc0788db9ba7dc8817106e55ea8f1bf721cc85
cejimenez0123 <christianjimenez0123@gmail.com>
2025-09-01 06:32:44 -0400

    links

--- src/container/LinksContainer.jsx
+++ src/container/LinksContainer.jsx
@@ -30,21 +30,17 @@ export default function LinksContainer(props){
      
             <h2 className="text-xl lora-bold mx-auto mt-12 text-emerald-700 mb-8 text-center">Keep up with us!<p>Support how you can!</p></h2>
             <ul className="md:mx-auto w-page-mobile-content md:w-page-content  mx-4">
-            <li>
-
-    <LinkNode url={"https://partiful.com/e/tMPilbVLft4gveSDg37M"}
-    image={flare}
-         title={"Plumbum x BXWriters Mixer Thursday Aug. 28th"}
-         description={"Mixer to bring the heart to tech, and the impact to art. Everyone is welcomed to form new connections."}/>
-         </li> 
-      
-    
-          
                 <li>  
                <LinkNode image={events}
        url={"https://plumbum.app"+Paths.calendar()}
                     title={"NYC Writing Calendar"}
                     description={"Need a place to be, find it in real life through our calendar of NYC and Virtual events."}/>
+                    </li>
+                    <li>
+               <LinkNode url={"https://plumbum.app"+Paths.discovery()}
+               image={logo}
+                    title={"Discover Plumbum"}
+                    description={"Check some recent shared work"}/>
                     </li>
                     <li>
                     <LinkNode url={"https://plumbum.app"+Paths.apply()}
@@ -53,20 +49,6 @@ export default function LinksContainer(props){
                     description={"Become a user and support us from the ground."}/>
                     
                 </li>
-                    <li>
-               <LinkNode url={"https://plumbum.app"+Paths.newsletter()}
-               image={logo}
-                    title={"Join the newsletter"}
-                    description={"Keep up with events, before you apply."}/>
-                    </li>
-              
-          
-                    {/* <li className="">
-                    <LinkNode url={"https://join.slack.com/t/plumbumwriters/shared_invite/zt-2zvkzyi02-dRlhqb0wvHAaU~~dUgh7hQ"}
-                    title={"Join our slack community"}
-                    image={"https://www.svgrepo.com/show/315526/slack.svg"}
-                    description={"Our slack community, our middle ground. A space for feedback, while we work on devleopment."}/>
-               </li> */}
                <li>
                <LinkNode url={"https://www.lulu.com/shop/sol-emilio/good-hell/paperback/product-45q8j8v.html?q=good+hell&page=1&pageSize=4"}
                     title={"Purchase literature written by the founder."}
@@ -91,12 +73,7 @@ image={"https://cdn.prod.website-files.com/5c14e387dab576fe667689cf/670f5a01229b
                     description={"Holding data costs money. Any support is appreicated at any amount."}/>
                </li>
            
-               <li>
-               <LinkNode url={"https://plumbum.app"+Paths.discovery()}
-               image={logo}
-                    title={"Discover Plumbum"}
-                    description={"Check some recent shared work"}/>
-                    </li>
+              
          
                
        
